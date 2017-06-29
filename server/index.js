682302346ec2b52589aa562871ba246046392c5f
Jess Poemape <jesspoemape@gmail.com>
2017-06-29 12:31:10 -0600

    completed project

--- server/index.js
+++ server/index.js
@@ -1,17 +1,34 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const session = require('express-session');
 const mc = require( `${__dirname}/controllers/messages_controller` );
+const createInitialSession = require('./middlewares/session');
+const filter = require('./middlewares/filter');
 
 const app = express();
 
 app.use( bodyParser.json() );
 app.use( express.static( `${__dirname}/../public/build` ) );
+app.use( session( {
+    secret: 'alsuydfg3732984hndb2649dbchoqpl2947hbkad9u23hdkjsdbewi839hsdbsdkwu',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {maxAge: 24*60*60*1000, secure: false}
+}));
+app.use( (req, res, next) => createInitialSession(req, res, next) ); 
+app.use( (req, res, next) => {
+    if (req.body && req.body.text) {
+        return filter(req, res, next)
+    }
+    next();
+} ); // this is instead of adding the middleware to each endpoint 
 
 const messagesBaseUrl = "/api/messages";
 app.post( messagesBaseUrl, mc.create );
 app.get( messagesBaseUrl, mc.read );
 app.put( `${messagesBaseUrl}`, mc.update );
 app.delete( `${messagesBaseUrl}`, mc.delete );
+app.get(messagesBaseUrl + '/history', mc.history);
 
 const port = 3000;
 app.listen( port, () => { console.log(`Server listening on port ${port}.`); } );
---
+++ server/middlewares/filter.js
@@ -0,0 +1,9 @@
+const notAllowed = [ 'farts', 'butts', 'fart', 'butt', 'poop', 'poo', 'pooping', 'pooped', 'pooper', 'farted', 'farting' ];
+
+module.exports = function (req, res, next) {
+    while ( notAllowed.find( word => req.body.text.includes(word) ) ) {
+  const badWord = notAllowed.find( word => req.body.text.includes(word) );
+  req.body.text = req.body.text.replace( badWord, '*'.repeat( badWord.length ) );
+}
+    next(); 
+}
